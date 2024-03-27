8ec70973d60c4e8f5d8c65ba7186d914acaa4610
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-03-27 23:31:59 +0530

    Learing REST

--- Mini-Project/index.js
+++ Mini-Project/index.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.set(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username : "SumitSingh",
+        content : "I love developing websites."
+    },
+    {
+        username : "apanacollege",
+        content : "We teach codding"
+    },
+    {
+        username : "Harpic",
+        content : "We sell Toilet cleaner"
+    }
+]
+
+app.get("/", (req, res) => {
+    res.send("C:\Learning_Express\Mini-Project\views\index.ejs", {posts});
+});
 
 app.listen(port, () => {
     console.log(`listening to port : ${port}`);
