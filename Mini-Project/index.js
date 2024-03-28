e3a5a75906bca82a78141ac0617885f8a578af3b
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-03-29 00:27:20 +0530

    Learing REST

--- Mini-Project/index.js
+++ Mini-Project/index.js
@@ -11,15 +11,18 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
-    {
+    {   
+        id : "1A",
         username : "SumitSingh",
         content : "I love developing websites."
     },
-    {
+    {   
+        id : "2B",
         username : "apanacollege",
         content : "We teach codding"
     },
-    {
+    {   
+        id : "3B",
         username : "Harpic",
         content : "We sell Toilet cleaner"
     },
@@ -27,6 +30,23 @@ let posts = [
 
 app.get("/posts", (req, res) => {
     res.render("index.ejs", {posts});
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let {username, content} = req.body;
+    posts.push({username, content});
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    
+    res.send("request working");
 });
 
 app.listen(port, () => {
