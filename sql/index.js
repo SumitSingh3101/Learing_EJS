ec27001ddaeae63adb29695a6588fc4bb7895c30
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-04-13 20:34:58 +0530

    Using facker for random data

--- sql/index.js
+++ sql/index.js
@@ -1,4 +1,14 @@
 const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "delta",
+    password: "@Sumit3101",
+});
 
 let getRandomUser = () => {
     return{
@@ -9,4 +19,21 @@ let getRandomUser = () => {
     };
 };
 
-console.log(getRandomUser());
+//Inserting New Data
+let q = "INSERT INTO USER(id, username, email, password) values ?";
+
+let data = [];
+for (let i = 1; i <= 100; i++){
+    data.push(getRandomUser()); //100 fake users
+}
+
+try{
+    connection.query(q, [data], (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+}catch(err) {
+    console.log(err);
+}
+
+connection.end();
