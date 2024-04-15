cf5698696d000e6a8dc82b86e10cc1fd4b4543ae
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-04-15 19:43:45 +0530

    Using sql data while routing

--- sql/index.js
+++ sql/index.js
@@ -19,21 +19,45 @@ let getRandomUser = () => {
     ];
 };
 
-//Inserting New Data
-let q = "INSERT INTO USER(id, username, email, password) values ?";
-
-let data = [];
-for (let i = 1; i <= 100; i++){
-    data.push(getRandomUser()); //100 fake users
-}
-
-try{
-    connection.query(q, [data], (err, result) => {
-        if (err) throw err;
-        console.log(result);
-    });
-}catch(err) {
-    console.log(err);
-}
-
 connection.end();
+
+app.get("/", (req, res) => {
+    let q = `SELECT count(*) FROM user`;
+    try{
+        connection.query(q,(err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send(result); 
+        });
+    }catch(err){
+        console.log(err);
+        res.send("some error in DataBase");
+    }
+});
+
+app.listen("8080", () => {
+    console.log("server is listening to port 8080");
+});
+
+
+
+
+
+
+
+//Inserting New Data
+// let q = "INSERT INTO USER(id, username, email, password) values ?";
+
+// let data = [];
+// for (let i = 1; i <= 100; i++){
+//     data.push(getRandomUser()); //100 fake users
+// }
+
+// try{
+//     connection.query(q, [data], (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// }catch(err) {
+//     console.log(err);
+// }
